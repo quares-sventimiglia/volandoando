48b103b3a3897ec25ddea2c9a655668de009aca7
seba-ventimiglia <sventimiglia@quaresitsolutions.com>
2021-02-23 09:08:15 -0300

    first commit

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,8 @@
-require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({typeDefs});
+
+server.listen().then(()=> {
+  console.log(`Server is running port 4000`)
+})
